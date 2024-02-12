eb057a57b77b0de790e9ce941dd696cb1ccffd82
sunvsps <69305747+sunvsps@users.noreply.github.com>
2024-02-12 17:44:52 +0700

    Update streamlit_app.py
    fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 
-streamlit.title('My Mom's New Healthy Dinner')
+streamlit.title('My Mom\'s New Healthy Dinner')
 
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
